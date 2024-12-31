05406d49cc32a8fa4b8abe7c3605f325ba490f29
osuiso-depot <85674477+osuiso-depot@users.noreply.github.com>
2024-12-31 23:13:38 +0900

    update extension and lora

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -21,14 +21,16 @@ PIP_PACKAGES=(
 EXTENSIONS=(
     # "https://github.com/deforum-art/sd-webui-deforum"
     "https://github.com/adieyal/sd-dynamic-prompts"
-    # "https://github.com/ototadana/sd-face-editor"
+    "https://github.com/ototadana/sd-face-editor"
     "https://github.com/AlUlkesh/stable-diffusion-webui-images-browser"
     "https://github.com/hako-mikan/sd-webui-regional-prompter"
     "https://github.com/Coyote-A/ultimate-upscale-for-automatic1111"
     "https://github.com/mcmonkeyprojects/sd-dynamic-thresholding"
     "https://github.com/Katsuyuki-Karasawa/stable-diffusion-webui-localization-ja_JP"
     "https://github.com/altoiddealer/--sd-webui-ar-plusplus"
+    "https://github.com/hako-mikan/sd-webui-lora-block-weight"
     # "https://github.com/DenOfEquity/forgeFlux_dualPrompt"
+    "https://github.com/zixaphir/Stable-Diffusion-Webui-Civitai-Helper"
 )
 
 CHECKPOINT_MODELS=(
@@ -38,10 +40,124 @@ CHECKPOINT_MODELS=(
     # "https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
     # "https://huggingface.co/rimOPS/noobBased/resolve/main/noobaiXLNAIXL_vPred10Version.safetensors"
     "https://huggingface.co/RedRayz/hikari_noob_1.0.0/resolve/main/Hikari_Noob_v-pred_1.0.0.safetensors"
+    "https://huggingface.co/rimOPS/IllustriousBased/resolve/main/vxpILXL_v12.safetensors"
 )
 
 LORA_MODELS=(
     #"https://civitai.com/api/download/models/16576"
+    # noob vpred
+    "https://civitai.com/api/download/models/1152003?type=Model&format=SafeTensor"
+    # Illustrious
+    # single hair bun
+    "https://civitai.com/api/download/models/1223585?type=Model&format=SafeTensor"
+    # セーラー服
+    "https://civitai.com/api/download/models/1222575?type=Model&format=SafeTensor"
+    # 体操服
+    "https://civitai.com/api/download/models/1179697?type=Model&format=SafeTensor"
+    # Retro Gym Shorts
+    "https://civitai.com/api/download/models/1166221?type=Model&format=SafeTensor"
+    # スク水
+    "https://civitai.com/api/download/models/1191090?type=Model&format=SafeTensor"
+    # スポーツブラ
+    "https://civitai.com/api/download/models/1166210?type=Model&format=SafeTensor"
+    # 貝殻ビキニ
+    "https://civitai.com/api/download/models/1183296?type=Model&format=SafeTensor"
+    # Reverse bunny suits
+    "https://civitai.com/api/download/models/995354?type=Model&format=SafeTensor"
+    # Bra embellishments enhancer
+    "https://civitai.com/api/download/models/924494?type=Model&format=SafeTensor"
+    # 裸リボン(lying)
+    "https://civitai.com/api/download/models/1168946?type=Model&format=SafeTensor"
+    # パイスラッシュ
+    "https://civitai.com/api/download/models/1179899?type=Model&format=SafeTensor"
+    # 隣の女の子
+    "https://civitai.com/api/download/models/1190625?type=Model&format=SafeTensor"
+    # [concept] 4 Finger Heart Hands
+    "https://civitai.com/api/download/models/1063667?type=Model&format=SafeTensor"
+    # Top Off(腕クロス脱ぎかけ)
+    "https://civitai.com/api/download/models/1014511?type=Model&format=SafeTensor"
+    # 胸ちら(後ろから)
+    "https://civitai.com/api/download/models/1191686?type=Model&format=SafeTensor"
+    # ぶりっ子ポーズ
+    "https://civitai.com/api/download/models/1190425?type=Model&format=SafeTensor"
+    # 腕組
+    "https://civitai.com/api/download/models/1056528?type=Model&format=SafeTensor"
+    # うずくまる-丸まる
+    "https://civitai.com/api/download/models/1168677?type=Model&format=SafeTensor"
+    # One-arm push-up
+    "https://civitai.com/api/download/models/1132005?type=Model&format=SafeTensor"
+    # 泡風呂
+    "https://civitai.com/api/download/models/1060187?type=Model&format=SafeTensor"
+    # Aru's Shocked Face
+    "https://civitai.com/api/download/models/1064292?type=Model&format=SafeTensor"
+    # Spread Armpit
+    "https://civitai.com/api/download/models/1103791?type=Model&format=SafeTensor"
+    # 臭いチェック
+    "https://civitai.com/api/download/models/1144981?type=Model&format=SafeTensor"
+    # Bed Invitation
+    "https://civitai.com/api/download/models/1149696?type=Model&format=SafeTensor"
+    # Showing the lining of the bra
+    "https://civitai.com/api/download/models/1130720?type=Model&format=SafeTensor"
+    # Self Suck
+    "https://civitai.com/api/download/models/1015748?type=Model&format=SafeTensor"
+    # [Pov] After Kiss
+    "https://civitai.com/api/download/models/1087076?type=Model&format=SafeTensor"
+    # from behind breast press top-down bottom-up
+    "https://civitai.com/api/download/models/1102220?type=Model&format=SafeTensor"
+    # Ohogao SDXL
+    "https://civitai.com/api/download/models/1192192"
+    # Silhouette Sex Pony
+    "https://civitai.com/api/download/models/929173"
+    # Ugly Fat Man
+    "https://civitai.com/api/download/models/1122234?type=Model&format=SafeTensor"
+    # Open Towel
+    "https://civitai.com/api/download/models/1154680?type=Model&format=SafeTensor"
+    # Paizuri Invitation
+    "https://civitai.com/api/download/models/1044024?type=Model&format=SafeTensor"
+    # Gliding Paizuri
+    "https://civitai.com/api/download/models/975923?type=Model&format=SafeTensor"
+    # フェラ素振り(指で輪っか)
+    "https://civitai.com/api/download/models/1181541?type=Model&format=SafeTensor"
+    # 踏みちん (LyCORIS)
+    "https://civitai.com/api/download/models/1200583?type=Model&format=SafeTensor"
+    # [Pov] FootWorship
+    "https://civitai.com/api/download/models/1171421?type=Model&format=SafeTensor"
+    # POV Lying on Viewer Handjob/Blowjob + Fingering
+    "https://civitai.com/api/download/models/1084837?type=Model&format=SafeTensor"
+    # POV Thighjob on Top
+    "https://civitai.com/api/download/models/1086822?type=Model&format=SafeTensor"
+    # Lying on Lap Blowjob
+    "https://civitai.com/api/download/models/1065263?type=Model&format=SafeTensor"
+    # 車内フェラ
+    "https://civitai.com/api/download/models/1187892?type=Model&format=SafeTensor"
+    # Vacuum Fellatio
+    "https://civitai.com/api/download/models/1069442?type=Model&format=SafeTensor"
+    # Reverse nursing Handjob
+    "https://civitai.com/api/download/models/1145452?type=Model&format=SafeTensor"
+    # cum on hands
+    "https://civitai.com/api/download/models/1182735?type=Model&format=SafeTensor"
+    # 素股
+    "https://civitai.com/api/download/models/1170084?type=Model&format=SafeTensor"
+    # 隠姦(ドア)
+    "https://civitai.com/api/download/models/1205243?type=Model&format=SafeTensor"
+    # 駅弁
+    "https://civitai.com/api/download/models/1158089?type=Model&format=SafeTensor"
+    # 獅子舞い
+    "https://civitai.com/api/download/models/1158271?type=Model&format=SafeTensor"
+    # Side Sex from Back
+    "https://civitai.com/api/download/models/1055765?type=Model&format=SafeTensor"
+    # 砧(きぬた) まんぐりSEX
+    "https://civitai.com/api/download/models/1183504?type=Model&format=SafeTensor"
+    # だいしゅきホールド
+    "https://civitai.com/api/download/models/1166885?type=Model&format=SafeTensor"
+    # doggy style kissing
+    "https://civitai.com/api/download/models/1204356?type=Model&format=SafeTensor"
+    # 乳首舐めスパイダー騎乗位(NoobAI-XL V-Pread 1.0)
+    "https://civitai.com/api/download/models/1208616?type=Model&format=SafeTensor"
+    # missionary position 吊り橋(腰持ち上げ)
+    "https://civitai.com/api/download/models/1202648?type=Model&format=SafeTensor"
+    # 御所車 woman on top(side,定点風)
+    "https://civitai.com/api/download/models/1197206?type=Model&format=SafeTensor"
 )
 
 VAE_MODELS=(
