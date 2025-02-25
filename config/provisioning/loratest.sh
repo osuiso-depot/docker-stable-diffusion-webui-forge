42ec700faf9c40e809582766ba4c22803dfa98ca
osuiso-depot <85674477+osuiso-depot@users.noreply.github.com>
2025-02-25 20:28:59 +0900

    update loratest.sh

--- config/provisioning/loratest.sh
+++ config/provisioning/loratest.sh
@@ -58,26 +58,7 @@ LORA_MODELS=(
     # loratest
     "https://huggingface.co/rimOPS/IllustriousBased_Lora_own/resolve/main/gafutncsh_v1.safetensors"
     "https://huggingface.co/rimOPS/IllustriousBased_Lora_own/resolve/main/gafutncsh_vxp_v1.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_flip_DebiasedLoss_kp/Lion250224_d32a12_26667e-8_b2_flip_DebiasedLoss_kp-000004.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_flip_DebiasedLoss_kp/Lion250224_d32a12_26667e-8_b2_flip_DebiasedLoss_kp-000005.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_flip_DebiasedLoss_kp/Lion250224_d32a12_26667e-8_b2_flip_DebiasedLoss_kp-000006.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_DebiasedLoss_ipNoise_kp/Lion250224_d32a12_26667e-8_b2_DebiasedLoss_ipNoise_kp-000004.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_DebiasedLoss_ipNoise_kp/Lion250224_d32a12_26667e-8_b2_DebiasedLoss_ipNoise_kp-000005.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_DebiasedLoss_ipNoise_kp/Lion250224_d32a12_26667e-8_b2_DebiasedLoss_ipNoise_kp-000006.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_DebiasedLoss_ipNoise_kp/Lion250224_d32a12_26667e-8_b2_DebiasedLoss_ipNoise_kp-000007.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_DebiasedLoss_ipNoise_kp/Lion250224_d32a12_26667e-8_b2_DebiasedLoss_ipNoise_kp-000008.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_swnorm1_kp/Lion250224_d32a12_26667e-8_b2_swnorm1_kp-000004.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_swnorm1_kp/Lion250224_d32a12_26667e-8_b2_swnorm1_kp-000005.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_swnorm1_kp/Lion250224_d32a12_26667e-8_b2_swnorm1_kp-000006.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_swnorm1_kp/Lion250224_d32a12_26667e-8_b2_swnorm1_kp-000007.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_swnorm1_kp/Lion250224_d32a12_26667e-8_b2_swnorm1_kp-000008.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_swnorm1_kp/Lion250224_d32a12_26667e-8_b2_swnorm1_kp.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_kp/Lion250224_d32a12_26667e-8_b2_kp-000004.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_kp/Lion250224_d32a12_26667e-8_b2_kp-000005.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_kp/Lion250224_d32a12_26667e-8_b2_kp-000006.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_kp/Lion250224_d32a12_26667e-8_b2_kp-000007.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_kp/Lion250224_d32a12_26667e-8_b2_kp-000008.safetensors"
-    "https://huggingface.co/rimOPS/tenchi_testlora/resolve/main/Lion250224_d32a12_26667e-8_b2_kp/Lion250224_d32a12_26667e-8_b2_kp.safetensors"
+    "https://huggingface.co/rimOPS/IllustriousBased_Lora_own/resolve/main/gafutncsh_ai_v1.safetensors"
 )
 
 VAE_MODELS=(
